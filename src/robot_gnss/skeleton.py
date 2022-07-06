05d7c7d696405f74cf2f48ad55ce3ebc21bfe1ed
Boris Gubanov <44787162+boris-gu@users.noreply.github.com>
2022-07-06 20:03:13 +0300

    Fix skeleton

--- src/robot_gnss/skeleton.py
+++ src/robot_gnss/skeleton.py
@@ -3,15 +3,17 @@
 # THE SKELETON OF THE MAIN PROGRAM
 
 import rospy
+import json
 from numpy import nan, isnan
 from sensor_msgs.msg import NavSatFix
 import gnss_math as gm
+from class1 import FourXFourBotControl
 
 # =====
 # ANGLE
 # =====
-latitude = 1
-longitude = 1
+latitude = None
+longitude = None
 
 
 def nav_cb(nsf):
@@ -19,35 +21,55 @@ def nav_cb(nsf):
     longitude = nsf.longitude
 
 
-rospy.init_node('robot', anonymous=True)
+rospy.init_node('tx2', anonymous=True)
 nav = rospy.Subscriber('fix', NavSatFix, nav_cb, queue_size=10)
 
 # ==============
 # GETTING POINTS
 # ==============
 # TODO: GET POINTS
-point_list = [[100, 100][110, 110][120, 120][130, 130]]  # загруженные точки
+with open('trajectory.json') as file:
+    point_list = json.load(file)
+rospy.loginfo(f'Trajectory loaded, number of points: {len(point_list)}')
 
 # ================
 # PASSING BY POINT
 # ================
-err_dist = 1  # m, allowable error
-err_angl = 15  # degrees, allowable error
+err_dist = 1  # m, allowable error TODO: выбрать оптимальный
+err_angl = 20  # degrees, allowable error TODO: выбрать оптимальный
 latitude_old = latitude
 longitude_old = longitude
 
+ffbc = FourXFourBotControl()
+rospy.loginfo('Wheel control loaded')
+
+
 for point in point_list:
-    while gm.distance((latitude, longitude), (point)) > err_dist:  # Until we reach the point
-        dist = gm.distance((latitude, longitude), (point))
-        angl = gm.angle((latitude_old, longitude_old),
-                        (latitude, longitude),
-                        (point))
-        # Updating *_old must be done at the beginning of the iteration,
-        # but after all the calculations where *_old is needed
-        latitude_old = latitude
-        longitude_old = longitude
-        if abs(angl) > err_angl:
-            # TODO: TURN
-            pass
-        # TODO: GO AHEAD
-        # TODO: SLEEP(1)
+    rospy.loginfo(f'Point: {point}')
+    while not rospy.is_shutdown():
+        if latitude is not None and longitude is not None:
+            dist = gm.distance((latitude, longitude), (point[0], point[1]))
+            # TODO: Фото, поиск лунок и сброс отравы
+            # Distance
+            if dist > err_dist:
+                angl = gm.angle((latitude_old, longitude_old),
+                                (latitude, longitude), (point[0], point[1]))
+                ffbc.set_wheels_frequency(20, 20)
+                rospy.sleep(5)
+                # Angle
+                if abs(angl) > err_angl:
+                    if abs(angl) < 60:
+                        angl_wheel = 30
+                    elif abs(angl) < 120:
+                        angl_wheel = 45
+                    elif abs(angl) <= 180:
+                        angl_wheel = 60
+                    if angl > 0:
+                        ffbc.set_wheels_frequency(angl_wheel, 0)
+                    else:
+                        ffbc.set_wheels_frequency(0, angl_wheel)
+                latitude_old = latitude
+                longitude_old = longitude
+            else:
+                break
+ffbc.set_wheels_frequency(0, 0)
