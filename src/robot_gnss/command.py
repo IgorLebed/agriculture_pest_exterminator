a9fc76140211bf25747b4c2f9f84a2852eb1f2c0
Boris Gubanov <44787162+boris-gu@users.noreply.github.com>
2022-08-15 18:03:51 +0300

    Test new cammand.py

--- src/robot_gnss/command.py
+++ src/robot_gnss/command.py
@@ -1,23 +1,67 @@
 #!/usr/bin/env python3
 
-from rosclass1 import FourXFourBotControl
 import rospy
+from std_msgs.msg import Int8
+from rosclass1 import FourXFourBotControl
 
-rospy.init_node('tx2', anonymous=True)
-ffbc = FourXFourBotControl()
+# ===============
+# Wheels callback
+# ===============
+left_front = 0
+left_back = 0
+right_front = 0
+right_back = 0
 
-while not rospy.is_shutdown():
-    left = int(input(' left: '))
-    if left > 120:
-        break
-    right = int(input('right: '))
-    time = float(input('time: '))
 
-    for i in range(5):
+def lf_cb(self, speed):
+    global left_front
+    left_front = speed
+
+
+def lb_cb(self, speed):
+    global left_back
+    left_back = speed
+
+
+def rf_cb(self, speed):
+    global right_front
+    right_front = speed
+
+
+def rb_cb(self, speed):
+    global right_back
+    right_back = speed
+
+
+def set_speed(time, left, right, last_left, last_right):
+    ffbc.set_wheels_frequency(left, right)
+    while (abs(left_front - last_left) < 5
+           or abs(left_back - last_left) < 5
+           or abs(right_front - last_right) < 5
+           or abs(right_back - last_right) < 5):
         ffbc.set_wheels_frequency(left, right)
-        # rospy.sleep(0.1)
-    rospy.sleep(time)
+        rospy.sleep(0.2)
 
-    for i in range(5):
-        ffbc.set_wheels_frequency(0, 0)
-        # rospy.sleep(0.1)
+
+if __name__ == '__main__':
+    lf_sub = rospy.Subscriber('/rpm_left_front', Int8, lf_cb, queue_size=10)
+    lb_sub = rospy.Subscriber('/rpm_left_back', Int8, lb_cb, queue_size=10)
+    rf_sub = rospy.Subscriber('/rpm_right_front', Int8, rf_cb, queue_size=10)
+    rb_sub = rospy.Subscriber('/rpm_right_back', Int8, rb_cb, queue_size=10)
+
+    rospy.init_node('tx2', anonymous=True)
+    ffbc = FourXFourBotControl()
+
+    while not rospy.is_shutdown():
+        left = int(input(' left: '))
+        if left > 120:
+            break
+        right = int(input('right: '))
+        time = float(input('time: '))
+
+        set_speed(time, left, right, 0, 0)
+        # ffbc.set_wheels_frequency(left, right)
+        rospy.sleep(time)
+
+        set_speed(0, 0, 0, left, right)
+        # ffbc.set_wheels_frequency(0, 0)
